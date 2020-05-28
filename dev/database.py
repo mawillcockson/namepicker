ea841bbf9aff5aff208961b1fb5526e33bd707c0
Matthew Willcockson <matthew@willcockson.family>
2020-05-27 20:30:51 -0500

    Finished Pony "first steps"

--- dev/database.py
+++ dev/database.py
@@ -1,8 +1,11 @@
 """
 Following the pony docs and tutorial
+It uses Python 2.x print statements
 """
-from pony import orm
 from typing import Callable
+
+from pony import orm
+
 
 def main() -> None:
     db = orm.Database()
@@ -25,7 +28,7 @@ def main() -> None:
 
     db.generate_mapping(create_tables=True)
 
-    space: Callable[[], None] = lambda:print("\n...\n")
+    space: Callable[[], None] = lambda: print("\n...\n")
     space()
 
     with orm.db_session():
@@ -37,10 +40,102 @@ def main() -> None:
 
         db.commit()
 
+    space()
+
     with orm.db_session():
         query = orm.select(p for p in Person if p.age > 21)
-        breakpoint()
-        print(*query)
+
+    space()
+
+    with orm.db_session():
+        try:
+            for p in query.order_by(Person.name):
+                orm.show(p)
+        except AttributeError as err:
+            print(
+                f"Can't use orm.show() on arbitrary objects, and the error messages are obtuse:\n{err}"
+            )
+        query.order_by(Person.name).show()
+
+    space()
+
+    with orm.db_session():
+        Car.select().show()
+
+    space()
+
+    with orm.db_session():
+        try:
+            Person.select(p for p in Person).show()
+        except TypeError as err:
+            print(
+                f"different select()s in different places have same semantic meaning, but different parameter types:\n{err}"
+            )
+
+    space()
+
+    with orm.db_session():
+        Person.select(lambda x: x.id + 1).show()
+        # Did not do what I expected
+        # Don't know what I expected,
+        # but not that
+
+    space()
+
+    with orm.db_session():
+        # This works
+        orm.select(c.id + 1 for c in Car).show()
+
+        # And this
+        orm.select(str(c.id) + "1" for c in Car).show()
+
+        try:
+            orm.select(str(lambda: c) for c in Car).show()
+        except NotImplementedError as err:
+            print(
+                f"Complex expressions should probably be iterated over though:\n{err}"
+            )
+
+    space()
+
+    # Better to iterate over it; e.g.
+    with orm.db_session():
+        query = orm.select(c for c in Car)
+
+        print(*(lambda: item for item in query), sep=", ")
+
+    space()
+
+    with orm.db_session():
+        # This is fine
+        Car.select()[:]
+
+        try:
+            print(Car.select()[1].owner)
+        except TypeError as err:
+            print(f"This doesn't work:\n{err}")
+
+        # This does work, though
+        print(Car.select()[:][1].owner)
+
+    space()
+
+    with orm.db_session():
+        # The cache is per-session, not shared
+        print(f"Car 1's owner: {Car[1].owner}")
+
+    space()
+
+    with orm.db_session():
+        mary = Person.select(lambda x: x.name == "Mary")[:][0]
+
+        try:
+            mary.age += 22.0
+        except TypeError as err:
+            print(f"This is a runtime type error that mypy can't catch:\n{err}")
+
+        orm.commit()
+
 
 if __name__ == "__main__":
     main()
