a4172e0f82276f6ab4df9d31f5c9e9c252f48181
Matthew Willcockson <matthew@willcockson.family>
2020-07-13 10:29:25 -0500

    Scrollable frame expands; need class
    So, as a class, it needs an obvious way to support adding objects to,
    and configuring the grid properties of the inner, scrollable frame.
    
    I also _really_ want it to handle automatically showing and hiding the
    scrollbars as needed, both for vertical and horizontal scrolling.
    
    Not sure how well I'll be able to do that for text, but we'll see.

--- namepicker/gui.py
+++ namepicker/gui.py
@@ -4,23 +4,27 @@ from typing import List, Dict
 from dataclasses import dataclass
 
 root = tkinter.Tk()
-root.grid_rowconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
 root.columnconfigure(0, weight=1)
 
 container = ttk.Frame(root)
-container.grid(sticky="N E S W")
+container.grid(column=0, row=0, sticky="N E S W")
+container.rowconfigure(0, weight=1)
+container.columnconfigure(0, weight=1)
 
 label1 = ttk.Label(container, text="Label 1")
 label1.grid(column=0, row=1, pady=5, sticky="N W")
 
 canvas_frame = ttk.Frame(container)
-canvas_frame.grid(column=0, row=0, pady=(5, 0), sticky="N W")
+canvas_frame.grid(column=0, row=0, pady=(5, 0), sticky="N S E W")
 canvas_frame.grid_rowconfigure(0, weight=1)
 canvas_frame.grid_columnconfigure(0, weight=1)
 canvas_frame.grid_propagate(False)
 
 canvas = tkinter.Canvas(canvas_frame, bg="yellow")
 canvas.grid(column=0, row=0, sticky="N E S W")
+canvas.columnconfigure(0, weight=1)
+canvas.rowconfigure(0, weight=1)
 
 scrollbar = ttk.Scrollbar(canvas_frame, orient="vertical", command=canvas.yview)
 scrollbar.grid(row=0, column=1, sticky="N S")
@@ -28,6 +32,9 @@ canvas.configure(yscrollcommand=scrollbar.set)
 
 scrollable_frame = ttk.Frame(canvas)
 canvas.create_window((0, 0), window=scrollable_frame, anchor="nw")
+scrollable_frame.grid(column=0, row=0, sticky="N S E W")
+scrollable_frame.columnconfigure(0, weight=1)
+scrollable_frame.rowconfigure(0, weight=1)
 
 string_vars = [tkinter.StringVar(value=f"Item {i} of 100") for i in range(1, 100+1)]
 entries = [ttk.Entry(scrollable_frame, textvariable=string_var) for string_var in string_vars]
@@ -37,15 +44,12 @@ for i, entry in enumerate(entries):
 scrollable_frame.update_idletasks()
 
 # 10% of the screen's smallest dimension
-sidelength = (min(root.winfo_screenwidth(), root.winfo_screenheight()) * 10) // 100
-print(sidelength)
+sidelength = (min(root.winfo_screenwidth(), root.winfo_screenheight()) * 30) // 100
 
 canvas_frame.config(width=sidelength, height=sidelength)
 canvas.config(scrollregion=canvas.bbox("all"))
 
 def update_canvas(event) -> None:
-    print(f"{event}\n{dir(event)}")
-    print(canvas.bbox("all"))
     scrollable_frame.update_idletasks()
     canvas.config(scrollregion=canvas.bbox("all"))
 
